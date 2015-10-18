0221d1fda5c8b5c68e1f391fcc1124f5a01108a3
Jiahui Xie <xtom32@gmail.com>
2015-10-18 10:47:12 -0600

    Added Profile and FriendList mockup classes

---
+++ mockup_class/Profile.java
@@ -0,0 +1,100 @@
+
+import android.graphics.Bitmap;
+
+/**
+ * Created by jiahui on 10/18/15.
+ */
+public class Profile {
+    private String firstName;
+    private String lastName;
+    private int age;
+    private String gender;
+    private String email;
+    private Bitmap selfie;
+    private String phoneNumber;
+    private String cityName;
+
+    // note only firstName and lastName are necessary attributes for profile
+    public Profile(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public Profile(String name) {
+        setName(name);
+    }
+
+    public String getName() {
+        return getFirstName() + " " + getLastName();
+    }
+
+    public void setName(String name) {
+        String[] nameArray = name.split(" ");
+        setFirstName(nameArray[0]);
+        setLastName(nameArray[1]);
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Bitmap getSelfie() {
+        return selfie;
+    }
+
+    public void setSelfie(Bitmap selfie) {
+        this.selfie = selfie;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+}
